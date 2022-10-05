1730b53dd2351100b255db86b1747fcdcfd791dd
iskandar777-dar <54578330+iskandar777-dar@users.noreply.github.com>
2022-10-06 00:35:51 +0700

    Update __main__.py

--- GreyCilik/__main__.py
+++ GreyCilik/__main__.py
@@ -198,7 +198,6 @@ from telethon.errors import (
 
 
 def stringsesi():
-query = update.callback_query
 query.data == "sesistring": (
 @Client.on_message(filters.private & ~filters.forwarded & filters.command(["generate", "gen", "string", "str"]))
 def sesi(_, msg):
