2849722ba033be32642980c25b7374bb0ec197e5
iskandar777-dar <54578330+iskandar777-dar@users.noreply.github.com>
2022-10-09 11:49:00 +0700

    Update __main__.py

--- GreyCilik/__main__.py
+++ GreyCilik/__main__.py
@@ -214,14 +214,14 @@ def generate_session(update, context, bot: Client, msg: Message, telethon=False)
 		]
 		elif query.data == "telethon":
 			query.message.edit_text(
-			ty = "ᴛᴇʟᴇᴛʜᴏɴ"
+			ty = "ᴛᴇʟᴇᴛʜᴏɴ",
 			reply_markup=InlineKeyboardMarkup(buttons),
 			parse_mode=ParseMode.MARKDOWN,
 			disable_web_page_preview=False )
 	
 		elif query.data == "pyrogram":
 			query.message.edit_text(
-			ty = "ᴩʏʀᴏɢʀᴀᴍ"  
+			ty = "ᴩʏʀᴏɢʀᴀᴍ", 
 			reply_markup=InlineKeyboardMarkup(buttons),
 			parse_mode=ParseMode.MARKDOWN,
 			disable_web_page_preview=False )
